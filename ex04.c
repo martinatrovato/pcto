6207b0c0f25d283016581817de6beaf0267bdda0
martinatrovato <155971242+martinatrovato@users.noreply.github.com>
2024-01-08 16:49:15 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float main () {
+    float a;
+    printf ("inserisci primo numero/n");
+    scanf ("%f", &a);
+printf ("primo numero inserito è: %f", a );
+
+float b;
+printf ("inserisci secondo numero/n");
+scanf ("%f", &b);
+printf ("secondo numero inserito è: %f", b);
+float c = a +b;
+printf ("a + b = c %f", c);
+return 0;
+
