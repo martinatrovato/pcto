6b050bcb1872f7b3dbbe624c133954d45d2ecc63
martinatrovato <155971242+martinatrovato@users.noreply.github.com>
2024-01-08 15:11:30 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () { 
+    int intero= 1;
+    printf ("intero: %d\n", intero);
+    
+    return 0;
+    
+    int A = 2;
+    printf ("intero A: %d\n", intero);
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main () {
+    float o =28;
+    printf ("float: %f", o);
+
+
+char e = 'E';
+printf ("char: %c", e);
+
+char l = 'L';
+printf ("char: %c", l);
+
+int q =11;
+printf ("intero: %c", q);
+
+
+
+
+}
+
+
