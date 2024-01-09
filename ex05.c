6e97b69a2a434ceda58ba4c66fc5e14590632582
martinatrovato <155971242+martinatrovato@users.noreply.github.com>
2024-01-09 10:20:23 +0100

    Add files via upload

--- ex05.c
+++ ex05.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main () {
-    int a ;
-     printf ("inserisci primo numero/n");
-    scanf ("%d/n", &a);
-printf ("primo numero inserito è: %d/n", a );
- int b;
- printf ("inserisci secondo numero/n");
-scanf ("%d/n", &b);
-printf ("secondo numero inserito è: %d/n", b);
+  int a; 
+  printf ("inseisci primo numero\n");
+  scanf ("%d\n", &a);
+  printf ("il numero inserito è : %d\n", a);
 
-int w = a%b;
-printf ("a modulo b = w, %d", w);
-return 0;
+  if(a % 2 == 0){
+    printf ("è pari\n");
+  }
+  else if (a %2 != 0){
+    printf("è dispari\n");
+  }
+
+
 
 }
